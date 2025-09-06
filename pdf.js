fcddccc4b4b09c4ff3cc060f7abd04edd0465a1e
Ranx <61233296+Ranepalip@users.noreply.github.com>
2025-09-06 10:19:41 +0300

    Add files via upload

---
+++ pdf.js
@@ -0,0 +1,11 @@
+pdfjs/
+  build/
+    pdf.js
+    pdf.worker.js
+  web/
+    viewer.html
+    viewer.js
+    viewer.css
+    cmaps/              (не обязательно, но желательно)
+    images/             (иконки для тулбара)
+    locale/             (переводы, опционально)
